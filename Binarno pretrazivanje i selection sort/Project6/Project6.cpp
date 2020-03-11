c508b1a184cbcfc60724a68dca355a098171a06b
markohonda1 <51539769+markohonda1@users.noreply.github.com>
2020-03-11 17:02:07 +0100

    binarno i selektion

---
+++ Binarno pretrazivanje i selection sort/Project6/Project6.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+	int n = 4;
+	int a = 6;
+	int M[6] = {1,6,2,7,3,4};
+	
+	int gornjaG=a-1, donjaG=0, srednjaV=0;
+
+	for (int i = 0; i < a-1; i++) {
+		for (int j = i+1; j <a; j++) {
+			if (M[i] > M[j]) {
+				int temp = M[i];
+				M[i] = M[j];
+				M[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < a; i++) {
+		printf("%d ", M[i]);
+	}
+
+	while (donjaG<=gornjaG) {
+		srednjaV = (donjaG + gornjaG) / 2;
+		if (srednjaV == n) {
+			printf("Broj se nalazi na %d mjestu", srednjaV);
+			exit(0);
+		}
+		if (n > srednjaV) 
+			donjaG = srednjaV + 1;
+		if (n < srednjaV)
+			gornjaG = srednjaV - 1;
+		
+			
+	}
+	printf("\nBroj ne postoji");
+}
+
